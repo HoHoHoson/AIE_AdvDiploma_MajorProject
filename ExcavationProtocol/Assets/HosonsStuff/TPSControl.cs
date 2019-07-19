d2001420fb9171c51779d81b616df64ccb297203
Unknown <hoson_zhong@hotmail.com>
2019-07-20 00:58:48 +1000

    Player Movement and Camera Rotation is fairly complete
    The camera springarm should be the final thing to add

--- ExcavationProtocol/Assets/HosonsStuff/TPSControl.cs
+++ ExcavationProtocol/Assets/HosonsStuff/TPSControl.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class TPSControl : MonoBehaviour
 {
+    public Transform playerModel;
+    public Transform cameraOrbit;
+
     public float playerSpeed = 200;
 
+    public float cameraSensitivity = 1;
+    public float cameraMaxPitch = 50;
+    public float cameraMinPitch = 50;
+
     private Rigidbody rb;
+
+    private float camera_yaw = 0;
+    private float camera_pitch = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +27,33 @@ public class TPSControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3();
-        float vertical_input = Input.GetAxis("Vertical");
-        float horizontal_input = Input.GetAxis("Horizontal");
+        /// Camera rotation ///
 
-        direction += Vector3.forward * vertical_input + Vector3.right * horizontal_input;
+        camera_yaw += Input.GetAxis("Mouse X") * cameraSensitivity;
+        camera_pitch += Input.GetAxis("Mouse Y") * cameraSensitivity;
+        // Clamp pitch value to prevent neck snapping
+        camera_pitch = Mathf.Clamp(camera_pitch, cameraMinPitch, cameraMaxPitch);
 
-        if (vertical_input != 0 && horizontal_input != 0)
-            Vector3.Normalize(direction);
+        cameraOrbit.rotation = Quaternion.Euler(-camera_pitch, camera_yaw, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        /// Player movement ///
+
+        // Removes the pitch rotation from cameraOrbit.forward so that it can be used for linear movement
+        // (doesn't change height if the camera is pitched)
+        Vector3 direction = cameraOrbit.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        direction.y = 0;
+        direction = direction.normalized;
+
+        // Rotate player model to the direction of movement 
+        if (direction != Vector3.zero)
+            playerModel.rotation = Quaternion.LookRotation(direction);
 
         direction *= playerSpeed * Time.deltaTime;
-        direction.y = rb.velocity.y;
 
+        direction.y = rb.velocity.y;
         rb.velocity = direction;
     }
 }
