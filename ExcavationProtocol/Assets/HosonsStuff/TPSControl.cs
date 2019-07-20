c3872836ee13fafc1f8f3da56fd5373ed793150c
Unknown <hoson_zhong@hotmail.com>
2019-07-20 21:37:27 +1000

    Added a simple camera collision system
    It still has some clipping issues though

--- ExcavationProtocol/Assets/HosonsStuff/TPSControl.cs
+++ ExcavationProtocol/Assets/HosonsStuff/TPSControl.cs
@@ -6,17 +6,22 @@ public class TPSControl : MonoBehaviour
 {
     public Transform playerModel;
     public Transform cameraOrbit;
-
+    public Transform playerCamera;
+    
     public float playerSpeed = 200;
 
     public float cameraSensitivity = 1;
     public float cameraMaxPitch = 50;
     public float cameraMinPitch = 50;
 
+    public Vector3 cameraOffset;
+    public float offsetMinDistance = 0.1f;
+
     private Rigidbody rb;
 
     private float camera_yaw = 0;
     private float camera_pitch = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +29,7 @@ public class TPSControl : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
         /// Camera rotation ///
 
@@ -35,6 +39,14 @@ public class TPSControl : MonoBehaviour
         camera_pitch = Mathf.Clamp(camera_pitch, cameraMinPitch, cameraMaxPitch);
 
         cameraOrbit.rotation = Quaternion.Euler(-camera_pitch, camera_yaw, 0);
+
+        /// Camera Collision ///
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraOrbit.position, -cameraOrbit.forward, out hit, -cameraOffset.z, ~(1 << 2)))
+            playerCamera.localPosition = new Vector3(0, 0, -Mathf.Clamp(hit.distance, offsetMinDistance, -cameraOffset.z));
+        else
+            playerCamera.localPosition = cameraOffset;
     }
 
     private void FixedUpdate()
