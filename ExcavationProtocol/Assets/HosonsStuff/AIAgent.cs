6960e015de172519a5c4d16dd77d098ff3c0f234
Hoson <hoson_zhong@hotmail.com>
2019-07-26 16:54:59 +1000

    Created base AI and State Machine

---
+++ ExcavationProtocol/Assets/HosonsStuff/AIAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AIAgent : MonoBehaviour
+{
+    enum EnemyType
+    {
+        BASIC = 0,
+        EXPLOSIVE,
+        BOSS
+    }
+
+    private int         m_health;
+    private int         m_damage;
+    private float       m_speed;
+    private EnemyType   m_type;
+    private GameObject  m_target;
+
+    public virtual void UpdateAgent() { }
+    public virtual void AttackAgent() { }
+}
---
+++ ExcavationProtocol/Assets/HosonsStuff/AIState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIState : MonoBehaviour
+{
+    public virtual void Initialise() { }
+    public virtual void UpdateState() { }
+    public virtual void Exit() { }
+}
