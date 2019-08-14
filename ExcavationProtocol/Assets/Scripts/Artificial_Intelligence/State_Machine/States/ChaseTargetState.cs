49b9183e85946c9d498f3a57e734131103a842b5
Hoson <hoson_zhong@hotmail.com>
2019-08-14 16:53:45 +1000

    Started to create custom AI prefabs

--- ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/State_Machine/States/ChaseTargetState.cs
+++ ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/State_Machine/States/ChaseTargetState.cs
@@ -10,12 +10,11 @@ public class ChaseTargetState : State
 
     public override void UpdateState(in Agent agent)
     {
-        Vector3 chase_dir = agent.GetTarget().transform.position - agent.transform.position;
-        chase_dir.y = 0;
-        chase_dir = chase_dir.normalized * agent.GetSpeed() * Time.deltaTime;
-        chase_dir.y = agent.GetRB().velocity.y;
+        Vector3 target_direction = agent.GetTarget().transform.position - agent.transform.position;
+        target_direction.y = 0;
+        target_direction = target_direction.normalized;
 
-        agent.GetRB().velocity = chase_dir;
+        agent.transform.rotation = Quaternion.LookRotation(target_direction);
     }
 
     public override void OnExit(in Agent agent)
