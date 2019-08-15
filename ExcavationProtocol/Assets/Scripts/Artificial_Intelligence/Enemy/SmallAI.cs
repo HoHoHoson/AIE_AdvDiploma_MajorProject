305019e8c3db951a653e92c2271d8b1ade0c8bba
Hoson <hoson_zhong@hotmail.com>
2019-08-15 11:58:42 +1000

    Prototype AI set

--- ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/Enemy/SmallAI.cs
+++ ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/Enemy/SmallAI.cs
@@ -2,30 +2,37 @@
 
 public class SmallAI : Agent
 {
+    [SerializeField]
+    private Transform   m_childedModel;
+    [SerializeField]
+    private int          m_healthValue = 3, m_damageValue = 5;
+    [SerializeField]
+    private float        m_speedValue = 300, m_leapAngle = 45, m_leapForce = 2;
+
     void Start()
     {
-        m_health = 3;
-        m_damage = 5;
-        m_speed = 300;
+        m_health    = m_healthValue;
+        m_damage    = m_damageValue;
+        m_speed     = m_speedValue;
+        m_type      = EnemyType.BASIC;
         m_rigidbody = GetComponent<Rigidbody>();
-        m_type = EnemyType.BASIC;
 
         m_state_machine = new StateMachine();
 
         // Got to make a proper target selecting function
         m_target = GameObject.Find("FPS_Controller");
 
-        State state = new IdleState();
+        State state = new LeapAtState(m_leapAngle, m_leapForce);
         state.AddTransition(new Transition("CHASETARGET",
             new Condition[] { new DistanceCondition(transform, m_target.transform, 3, DistanceCondition.Comparator.GREATER) }));
         m_state_machine.AddState(state);
 
         state = new ChaseTargetState();
-        state.AddTransition(new Transition("IDLE",
+        state.AddTransition(new Transition("LEAPAT",
             new Condition[] { new DistanceCondition(transform, m_target.transform, 3, DistanceCondition.Comparator.LESS) }));
         m_state_machine.AddState(state);
 
-        m_state_machine.InitiateStateMachine(this, "IDLE");
+        m_state_machine.InitiateStateMachine(this, "CHASETARGET");
     }
 
     private void Update()
--- ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/State_Machine/States/ChaseTargetState.cs
+++ ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/State_Machine/States/ChaseTargetState.cs
@@ -4,10 +4,6 @@ public class ChaseTargetState : State
 {
     public ChaseTargetState() { m_index = "CHASETARGET"; }
 
-    public override void OnInitialise(in Agent agent)
-    {
-    }
-
     public override void UpdateState(in Agent agent)
     {
         Vector3 target_direction = agent.GetTarget().transform.position - agent.transform.position;
@@ -15,9 +11,13 @@ public class ChaseTargetState : State
         target_direction = target_direction.normalized;
 
         agent.transform.rotation = Quaternion.LookRotation(target_direction);
-    }
 
-    public override void OnExit(in Agent agent)
-    {
+        Vector3 new_velocity = target_direction * agent.GetSpeed() * Time.deltaTime;
+        new_velocity.y = agent.GetRB().velocity.y;
+
+        //RaycastHit hit;
+        //if (Physics.CapsuleCast(agent.transform.position, 0.1f, out hit))
+
+        agent.GetRB().velocity = new_velocity;
     }
 }
