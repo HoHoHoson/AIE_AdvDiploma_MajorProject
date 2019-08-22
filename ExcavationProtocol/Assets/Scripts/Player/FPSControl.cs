ed76355704002dbebe0e8825f4563d82024f3d64
Tim <tlangford203@gmail.com>
2019-08-22 16:48:40 +1000

    skill 1 has changed now
    it does good things

--- ExcavationProtocol/Assets/Scripts/Player/FPSControl.cs
+++ ExcavationProtocol/Assets/Scripts/Player/FPSControl.cs
@@ -27,13 +27,14 @@ public class FPSControl : MonoBehaviour
 
     private bool has_jumped;
 
-    public float skill_1_radius = 5.0f;
-    public float skill_1_power = 10.0f;
+    public int skill_damage = 1;
+
+    public GameObject bomb, g_throw_point;
+    public float throw_force = 100;
 
     public float skill_2_radius = 5.0f;
     public float skill_2_power = 10.0f;
 
-    public int skill_damage = 1;
 
     #endregion
 
@@ -60,6 +61,8 @@ public class FPSControl : MonoBehaviour
     private LineRenderer laser_line;
     private float next_fire;
 
+
+    bool unlocked_mouse;
     #endregion
 
     // Start is called before the first frame update
@@ -67,7 +70,6 @@ public class FPSControl : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
         animator = GetComponent<Animator>();
 
         m_player_rb = GetComponent<Rigidbody>();
@@ -84,12 +86,18 @@ public class FPSControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && GroundPlayer() == true)
             StartCoroutine(Jumping());
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && unlocked_mouse == false)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+            unlocked_mouse = true;
         }
-
+        if (Input.GetKeyDown(KeyCode.Escape) && unlocked_mouse == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            unlocked_mouse = false;
+        }
     }
 
     void LateUpdate()
@@ -160,27 +168,12 @@ public class FPSControl : MonoBehaviour
     /// </summary>
     public void SkillActive1()
     {
-
-        Vector3 explosionPos = transform.position;
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, skill_1_radius);
-
-        foreach (Collider hit in colliders)
-        {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-            if (rb == null)
-            {
-                rb = hit.GetComponentInChildren<Rigidbody>();
-            }
-            if (rb != null && hit.transform.tag != "Player")
-            {
-                rb.AddExplosionForce(skill_1_power, explosionPos, skill_1_radius, 3.0f);
-                if(hit.GetComponent<Agent>() != null)
-                {
-                    hit.GetComponent<Agent>().TakeDamage(skill_damage);
-                }
-            }
-        }
+        GameObject expl = Instantiate(bomb);
+        expl.transform.position = g_throw_point.transform.position;
+        expl.GetComponent<Rigidbody>().AddForce(g_throw_point.transform.forward * throw_force, ForceMode.Impulse);
     }
+
+   
 
     /// <summary>
     /// Performs skill 2 ( Disengage to point behind Player )
---
+++ ExcavationProtocol/Assets/Scripts/TimsStuff/Tims scripts/Grenade.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    FPSControl script_fps;
+    public float bomb_timer = 2.0f;
+    bool has_exploded;
+    public float skill_1_radius = 5.0f;
+    public float skill_1_power = 10.0f;
+    private int skill_damage;
+    float countdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script_fps = FindObjectOfType<FPSControl>();
+        skill_damage = script_fps.skill_damage;
+        countdown = bomb_timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0f && !has_exploded)
+        {
+            Explode();
+        }
+    }
+
+    public void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, skill_1_radius);
+
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb == null)
+            {
+                rb = hit.GetComponentInChildren<Rigidbody>();
+            }
+            if (rb != null && hit.transform.tag != "Player")
+            {
+                rb.AddExplosionForce(skill_1_power, explosionPos, skill_1_radius, 3.0f);
+                if (hit.GetComponent<Agent>() != null)
+                {
+                    hit.GetComponent<Agent>().TakeDamage(skill_damage);
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+}
