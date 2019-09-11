ee4ba31ba050799ac05697a53156ad0dcd0dd8fb
Hoson <hoson_zhong@hotmail.com>
2019-09-11 16:55:18 +1000

    Basic BossAI logic

---
+++ ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/Enemy/BossAI.cs
@@ -0,0 +1,18 @@
+﻿
+public class BossAI : Agent
+{
+    public override void InitialiseAgent(in Blackboard blackboard)
+    {
+        base.InitialiseAgent(blackboard);
+
+        m_type = EnemyType.BOSS;
+    }
+
+    protected override void InitialiseStateMachine()
+    {
+        State state = new SeekTargetState(this, m_blackboard, 30);
+        m_state_machine.AddState(state);
+
+        m_state_machine.InitiateStateMachine(this, "SEEKTARGET");
+    }
+}
