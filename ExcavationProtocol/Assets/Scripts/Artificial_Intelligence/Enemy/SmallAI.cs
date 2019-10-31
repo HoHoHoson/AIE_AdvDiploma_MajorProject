f6dd903b09ce6a4d391aaa80fe60be14d2e74c1d
Hoson <hoson_zhong@hotmail.com>
2019-10-31 16:51:53 +1100

    SmallAI will now randomly leap, modifiable in the inspector

--- ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/Enemy/SmallAI.cs
+++ ExcavationProtocol/Assets/Scripts/Artificial_Intelligence/Enemy/SmallAI.cs
@@ -2,10 +2,15 @@
 
 public class SmallAI : Agent
 {
-    [SerializeField] private float m_playerSeekRadius   = 10;
-    [SerializeField] private float m_leapRadius         = 5;
+    [SerializeField] private float  m_playerSeekRadius  = 10;
+    [SerializeField] private float  m_leapRadius        = 5;
+    [SerializeField] private float  m_randomLeapChance  = 0.01f;
+    [SerializeField] private int    m_tickRate          = 10;
 
     private Animator m_animator;
+
+    private float m_tick_time;
+    private float m_random_leap_timer = 0;
 
     public override void InitialiseAgent(in Blackboard blackboard)
     {
@@ -13,16 +18,42 @@ public class SmallAI : Agent
 
         m_type = EnemyType.BASIC;
         m_animator = GetComponent<Animator>();
+        m_tick_time = 1f / m_tickRate;
     }
 
     public override void UpdateAgent()
     {
         base.UpdateAgent();
 
-        if (m_state_machine.GetCurrentState() is LeapAtState)
+        State current_state = m_state_machine.GetCurrentState();
+        bool state_found = false;
+
+        if (!state_found || current_state is SeekTargetState)
+        {
+            m_random_leap_timer += Time.deltaTime;
+            state_found = true;
+        }
+
+        if (!state_found || current_state is LeapAtState)
+        {
             m_animator.SetBool("Attack", true);
+            state_found = true;
+        }
         else
             m_animator.SetBool("Attack", false);
+    }
+
+    public bool RandomLeapCheck()
+    {
+        if (m_random_leap_timer > m_tick_time)
+        {
+            m_random_leap_timer -= m_tick_time;
+
+            if (Random.value <= m_randomLeapChance)
+                return true;
+        }
+
+        return false;
     }
 
     protected override void InitialiseStateMachine()
@@ -34,7 +65,8 @@ public class SmallAI : Agent
         state = new SeekTargetState(this, m_blackboard, m_playerSeekRadius);
         // Leaps at the targets face when in range
         state.AddTransition(new Transition("LEAPAT",
-            new Condition[] { new CompareCondition(this, m_leapRadius, CompareCondition.Comparator.LESS),
+            new Condition[] { new BoolCondition(RandomLeapCheck),
+                              new CompareCondition(this, m_leapRadius, CompareCondition.Comparator.LESS),
                               new BoolCondition(GetCliffLeap) }));
         m_state_machine.AddState(state);
 
